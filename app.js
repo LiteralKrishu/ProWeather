e67926c08f9dbb289f1676ed1da401a2d45428cb
LiteralKrishu <130468469+LiteralKrishu@users.noreply.github.com>
2025-10-07 23:26:53 +0530

    Production Webapp

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // API Configuration
-const API_KEY = 'YOUR_API_KEY';
+const API_KEY = '09bf19336f963a5b88058d0882551a19';
 const BASE_URL = 'https://api.openweathermap.org/data/2.5';
 const GEOCODING_URL = 'https://api.openweathermap.org/geo/1.0/direct';
 const ICON_URL = 'https://openweathermap.org/img/wn/';
